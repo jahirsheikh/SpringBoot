455a2fcd9f3ff46823b1274400b4b3366461919e
jahirsheikh <jahirsheikh14@gmail.com>
2024-01-11 13:19:22 +0600

    Project Detalis

---
+++ project_daynamic_pos/project_daynamic_pos/src/main/java/com/jahir/project_daynamic_pos/controller/Home.java
@@ -0,0 +1,15 @@
+package com.jahir.project_daynamic_pos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class Home {
+
+    @GetMapping("/")
+    public String home(){
+
+        return "home";
+    }
+}
